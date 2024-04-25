d6206765d183ee87cd2969962520217756539e86
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2024-04-25 12:48:03 -0300

    Subindo para Github

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿string permission = "User";
+int level = 19;
+
+if (permission.Contains("Manager"))
+{
+    Console.WriteLine(level >= 20 ? "Contact an Admin for access" : "You do not have sufficient privileges.");
+}
+else if (permission.Contains("Admin"))
+{
+    Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user");
+}
+else
+    Console.WriteLine("You do not have sufficient privileges.");
+
