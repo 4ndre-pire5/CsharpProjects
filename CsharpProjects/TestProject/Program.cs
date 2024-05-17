e999ac9f69cc2ecebddfc0ab25ac6ba776395fa0
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2024-05-17 16:23:51 -0300

    Estudando

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,39 @@
-﻿string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
+﻿// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] phrase = pangram.Split(' ');
+
+// string[] newPhrase = new string[phrase.Length];
+
+// for (int i = 0; i < phrase.Length; i++)
+// {
+//     char[] letters = phrase[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newPhrase[i] = new string(letters);
+// }
+
+// string result = String.Join(" ", newPhrase);
+// Console.WriteLine(result);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] arrayId = orderStream.Split(',');
+Array.Sort(arrayId);
+
+for (int i = 0; i < arrayId.Length; i++)
+{
+    if (arrayId[i].Length != 4)
+    {
+        Console.WriteLine($"{arrayId[i]} \t- Error");
+    }
+    else
+    {
+        Console.WriteLine(arrayId[i]);
+    }
+}
+
+
+
+
+
+
+
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6206765d183ee87cd2969962520217756539e86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b12058c961ec81945c35b1ed8898c7402ae465e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
